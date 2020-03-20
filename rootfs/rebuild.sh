2ff739b12ea78158758c0623bc88af8cf11b032d
steven.liu <steven.liu@paytm.com>
2020-03-20 22:32:35 +0800

    1.Moved busybox position. 2. Added ramdisk file compiled with another old ubuntu linux

--- rootfs/rebuild.sh
+++ rootfs/rebuild.sh
@@ -1,8 +1,8 @@
 sudo umount ./root
-dd if=/dev/zero of=/tmp/linux2.6.34-ramdisk.img bs=1M count=32
+dd if=/dev/zero of=/tmp/linux2.6.34-ramdisk.img bs=1M count=8
 mkfs.ext3 /tmp/linux2.6.34-ramdisk.img
 sudo mount /tmp/linux2.6.34-ramdisk.img ./root
-sudo cp -R ../busybox/busybox-1.31.1/_install/* ./root/
+sudo cp -R ../ramdisk/* ./root/
 sudo chown root:root ./root/* -R
 sudo umount ./root
 cp /tmp/linux2.6.34-ramdisk.img ./
